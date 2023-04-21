9544faecccd6fdfdf4b2bc64ac3575f02216f40c
Yann Hodique <yhodique@google.com>
2023-04-21 18:50:59 +0200

    fix info workarounds

--- info-workarounds.el
+++ info-workarounds.el
@@ -1,10 +1,12 @@
 ;;; info-workarounds.el --- Description -*- lexical-binding: t; -*-
 ;;; This file allows the `info` function to find other `info` files in the system.
 
-;;; For non-NixOS GNU/Linux
-(push "/usr/share/info" Info-directory-list)
+(eval-after-load 'info
+  '(progn
+     ;;; For non-NixOS GNU/Linux
+     (push "/usr/share/info" Info-directory-list)
 
-;;; For NixOS
-(push "/run/current-system/sw/share/info" Info-directory-list)
+     ;;; For NixOS
+     (push "/run/current-system/sw/share/info" Info-directory-list)
 
-(push "~/.nix-profile/share/info" Info-directory-list)
+     (push "~/.nix-profile/share/info" Info-directory-list)))
